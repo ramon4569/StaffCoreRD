c37be889e2e0e4b0db408a2bc5b2925e940cb47d
ranmanew <ranmanew@DESKTOP-T9HTFSS>
2026-07-24 11:11:56 -0400

    Agregar archivos de proyecto.

---
+++ StaffCoreRD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StaffCoreRD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
