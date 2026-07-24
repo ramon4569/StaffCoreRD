fdedce5665cf017960436d1f83e6b11829ae2141
ranmanew <ranmanew@DESKTOP-T9HTFSS>
2026-07-24 11:46:59 -0400

    feat: implementar Index de Staff (listado + total nómina) (Módulo 7)
    - Crear StaffController con [Authorize] y acción Index
    - Listar personal ordenado por nombre desde StaffDbContext
    - Crear vista Index.cshtml con tabla y tfoot con total de nómina
    - Mostrar botones Editar/Eliminar/Crear condicionados por rol

---
+++ StaffCoreRD/Controllers/StaffController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StaffCoreRD.Data;
+
+namespace StaffCoreRD.Controllers;
+
+[Authorize] // cualquier usuario logueado puede ver el listado; Módulo 11 refina esto por acción
+public class StaffController : Controller
+{
+    private readonly StaffDbContext _context;
+
+    public StaffController(StaffDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: /Staff
+    public async Task<IActionResult> Index()
+    {
+        var personal = await _context.Personal
+            .OrderBy(s => s.Nombre)
+            .ToListAsync();
+
+        return View(personal);
+    }
+}
